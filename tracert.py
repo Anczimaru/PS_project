c8706c36912a61eba6ae10851f6729c22817fe2d
Anczimaru <anczi101@gmail.com>
2019-06-24 21:40:49 +0200

    work on icmp

--- tracert.py
+++ tracert.py
@@ -37,6 +37,56 @@ class TraceRoute():
         self.recv_port = None #placeholder for receiver
         self.packet = None #placeholder
 
+    
+        def checksum(package):
+            """
+            checksum taken from https://gitlab.com/mezantrop/sp_ping/blob/master/sp_ping.py#L137
+            """
+            packet_len = len(package)
+            sum = 0
+            for i in range(0, packet_len, 2):
+                if i + 1 < packet_len:
+                    # Fold 2 neighbour bytes into a number and add it to the sum
+                    sum += package[i] + (package[i + 1] << 8)
+                else:
+                    # If there is an odd number of bytes, fake the second byte
+                    sum += package[i] + 0
+            # Add carry bit to the sum
+            sum = (sum >> 16) + (sum & 0xffff)
+            # Truncate to 16 bits and return the checksum
+            return ~sum & 0xffff
+
+
+        def create_packet():
+            """
+            Creatin of packet taken from https://gitlab.com/mezantrop/sp_ping/blob/master/sp_ping.py#L137
+            """
+             # Packet header definition
+            iphdr_len = 60                  # Max is 60, but in our case for IPv4 it should be 20 bytes. Adjust it after recv()
+            icmphdr_len = 8                 # ICMP header length is 8 bytes
+            icmp_type_request = 8           # ICMP IPv4 ECHO_REQUEST
+            icmp_type_reply = 0             # ICMP IPv4 ECHO_REPLY
+
+            icmp_code = 0
+            icmp_checksum = 0
+            icmp_id = os.getpid() & 0xffff  # Generate ID field using PID converted to 16 bit
+            # Some ICMP payload examples. Do not make them too long:
+            icmp_data = b'\x50\x49\x4E\x47\x2D\x50\x4F\x4E\x47\x20\x46\x52\x4F\x4D' \
+                        b'\x20\x5A\x4D\x45\x59\x32\x30\x30\x30\x30\x40\x59\x41\x48' \
+                        b'\x4F\x4F\x2E\x43\x4F\x4D'
+            # icmp_data = b'12345678' + b'1234567890' * 4
+
+            data_len = len(icmp_data)
+
+            send_timestamp = time.time()    # Packet creation time
+            out_packet = struct.pack('BBHHHQ{}s'.format(data_len), icmp_type_request, icmp_code,
+                                     icmp_checksum, icmp_id, sequence, int(send_timestamp), icmp_data)
+            icmp_checksum = clk_chksum(out_packet)
+            out_packet = struct.pack('BBHHHQ{}s'.format(data_len), icmp_type_request, icmp_code,
+                                     icmp_checksum, icmp_id, sequence, int(send_timestamp), icmp_data)
+            return out_packet
+
+
     def create_ports(self, ttl=30):
         """
         Create and keep ports updated
@@ -110,52 +160,3 @@ class TraceRoute():
                 if last_addr == self.dest_addr:
                     print("Final destination reached")
                     break
-
-
-        def checksum(package):
-            """
-            checksum taken from https://gitlab.com/mezantrop/sp_ping/blob/master/sp_ping.py#L137
-            """
-            packet_len = len(package)
-            sum = 0
-            for i in range(0, packet_len, 2):
-                if i + 1 < packet_len:
-                    # Fold 2 neighbour bytes into a number and add it to the sum
-                    sum += package[i] + (package[i + 1] << 8)
-                else:
-                    # If there is an odd number of bytes, fake the second byte
-                    sum += package[i] + 0
-            # Add carry bit to the sum
-            sum = (sum >> 16) + (sum & 0xffff)
-            # Truncate to 16 bits and return the checksum
-            return ~sum & 0xffff
-
-
-        def create_packet():
-            """
-            Creatin of packet taken from https://gitlab.com/mezantrop/sp_ping/blob/master/sp_ping.py#L137
-            """
-             # Packet header definition
-            iphdr_len = 60                  # Max is 60, but in our case for IPv4 it should be 20 bytes. Adjust it after recv()
-            icmphdr_len = 8                 # ICMP header length is 8 bytes
-            icmp_type_request = 8           # ICMP IPv4 ECHO_REQUEST
-            icmp_type_reply = 0             # ICMP IPv4 ECHO_REPLY
-
-            icmp_code = 0
-            icmp_checksum = 0
-            icmp_id = os.getpid() & 0xffff  # Generate ID field using PID converted to 16 bit
-            # Some ICMP payload examples. Do not make them too long:
-            icmp_data = b'\x50\x49\x4E\x47\x2D\x50\x4F\x4E\x47\x20\x46\x52\x4F\x4D' \
-                        b'\x20\x5A\x4D\x45\x59\x32\x30\x30\x30\x30\x40\x59\x41\x48' \
-                        b'\x4F\x4F\x2E\x43\x4F\x4D'
-            # icmp_data = b'12345678' + b'1234567890' * 4
-
-            data_len = len(icmp_data)
-
-            send_timestamp = time.time()    # Packet creation time
-            out_packet = struct.pack('BBHHHQ{}s'.format(data_len), icmp_type_request, icmp_code,
-                                     icmp_checksum, icmp_id, sequence, int(send_timestamp), icmp_data)
-            icmp_checksum = clk_chksum(out_packet)
-            out_packet = struct.pack('BBHHHQ{}s'.format(data_len), icmp_type_request, icmp_code,
-                                     icmp_checksum, icmp_id, sequence, int(send_timestamp), icmp_data)
-            return out_packet
